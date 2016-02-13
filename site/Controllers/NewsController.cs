e5a5b1cb97312b0c1ab11030eafa80fbd8702f2a
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-13 22:50:56 +0300

    add news controller

---
+++ site/Controllers/NewsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace site.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new [] { "value1", "value2" };
+        }
+    }
+}
