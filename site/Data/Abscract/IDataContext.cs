8acdae93204bcaec162db7c7adb1f5cc813f90ee
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-14 14:20:47 +0300

    implement data context

---
+++ site/Data/Abscract/IDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.Entity;
+using site.Domain;
+
+namespace site.Data.Abscract
+{
+    public interface IDataContext
+    {
+        DbSet<News> News { get; set; }
+    }
+}
---
+++ site/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Entity;
+using site.Data.Abscract;
+using site.Domain;
+
+namespace site.Data
+{
+    public class DataContext : DbContext, IDataContext
+    {
+        public DbSet<News> News { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<News>()
+                .ToTable("NS_News")
+                .HasOne(x => x.ShowplaceCategory)
+                .WithMany();
+
+            modelBuilder
+                .Entity<ShowplaceCategory>()
+                .ToTable("NS_ShowplaceCategory")
+                .HasOne(x => x.Showplace)
+                .WithMany();
+
+            modelBuilder
+                .Entity<Showplace>()
+                .ToTable("NS_Showplace");
+        }
+    }
+}
