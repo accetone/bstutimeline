388e44b896676a7e029e8e7cbb8c985f6473598e
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-14 14:21:07 +0300

    implement news repository

---
+++ site/Data/Abscract/INewsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using site.Domain;
+
+namespace site.Data.Abscract
+{
+    public interface INewsRepository
+    {
+        IQueryable<News> GetAll();
+    }
+}
---
+++ site/Data/NewsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using site.Data.Abscract;
+using site.Domain;
+
+namespace site.Data
+{
+    public class NewsRepository : INewsRepository
+    {
+        private IDataContext Db { get; }
+
+        public NewsRepository(IDataContext db)
+        {
+            Db = db;
+        }
+
+        public IQueryable<News> GetAll()
+        {
+            return Db.News;
+        } 
+    }
+}
