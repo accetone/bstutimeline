807c02a71bede20d8c0e23e02478ae7a38b1497b
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-17 12:18:21 +0300

    implement get method for news controller

--- site/Controllers/NewsController.cs
+++ site/Controllers/NewsController.cs
@@ -1,15 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNet.Mvc;
+using site.Data.Abscract;
+using site.Dto;
 
 namespace site.Controllers
 {
     [Route("api/[controller]")]
     public class NewsController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private INewsService NewsService { get; }
+        private IImageService ImageService { get; }
+
+        public NewsController(INewsService newsService, IImageService imageService)
         {
-            return new [] { "value1", "value2" };
+            NewsService = newsService;
+            ImageService = imageService;
+        }
+
+        [HttpGet]
+        public IEnumerable<NewsDto> Get(int skip, int take)
+        {
+            if (take == 0) take = 10;
+
+            return NewsService
+                .ReadChunk(skip, take)
+                .Select(x => new NewsDto(x, ImageService))
+                .ToList();
         }
     }
 }
