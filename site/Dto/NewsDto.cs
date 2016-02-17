c3ba4f2b1e9ac6968f3c854a8aabeca0aed77cab
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-17 12:08:28 +0300

    implement news DTO

---
+++ site/Dto/NewsDto.cs
@@ -0,0 +1,29 @@
+﻿using site.Data.Abscract;
+using site.Domain;
+
+namespace site.Dto
+{
+    public class NewsDto
+    {
+        public int Id;
+        public string Title;
+        public string Place;
+        public string Date;
+        public string Thumb;
+        public int Views;
+        public string Url;
+
+        public NewsDto(News news, IImageService imageService)
+        {
+            Id = news.Id;
+            Title = news.Title;
+            Place = news.ShowplaceCategory.Showplace.Name;
+            Date = news.EndDate == null 
+                ? news.StartDate.ToString("dd.MM.yy")
+                : $"{news.StartDate.ToString("dd.MM.yy")} - {news.EndDate.Value.ToString("dd.MM.yy")}";
+            Thumb = imageService.GetThumbUrl(news);
+            Views = news.Views;
+            Url = $"https://{news.ShowplaceCategory.Showplace.Host}{news.Ref}";
+        }
+    }
+}
