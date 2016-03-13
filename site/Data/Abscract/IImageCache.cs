a8f4062f36abcfbf6dc6a21e3f22cb9ce0aaf81c
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-13 20:14:45 +0300

    implement image cache

---
+++ site/Data/Abscract/IImageCache.cs
@@ -0,0 +1,10 @@
+﻿using site.Domain;
+
+namespace site.Data.Abscract
+{
+    public interface IImageCache
+    {
+        string GetThumbUrl(News news);
+        void SetThumbUrl(News news, string url);
+    }
+}
---
+++ site/Data/ImageCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using site.Data.Abscract;
+using site.Domain;
+
+namespace site.Data
+{
+    public class ImageCache : IImageCache
+    {
+        private Dictionary<int, string> ThumbUrls { get; }
+
+        public ImageCache()
+        {
+            ThumbUrls = new Dictionary<int, string>();
+        }
+
+        public string GetThumbUrl(News news)
+        {
+            return ThumbUrls.ContainsKey(news.Id) 
+                ? ThumbUrls[news.Id]
+                : null;
+        }
+
+        public void SetThumbUrl(News news, string url)
+        {
+            ThumbUrls[news.Id] = url;
+        }
+    }
+}
