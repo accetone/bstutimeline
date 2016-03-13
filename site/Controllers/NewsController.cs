a2ef2f0da2d6bd72ca18eb8f65a99b7b25d6db41
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-13 20:18:49 +0300

    refactor news controller

--- site/Controllers/NewsController.cs
+++ site/Controllers/NewsController.cs
@@ -6,20 +6,20 @@ using site.Dto;
 
 namespace site.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class NewsController : Controller
     {
         private INewsService NewsService { get; }
         private IImageService ImageService { get; }
 
         public NewsController(INewsService newsService, IImageService imageService)
-        {
+        {  
             NewsService = newsService;
             ImageService = imageService;
         }
 
         [HttpGet]
-        public IEnumerable<NewsDto> Get(int skip, int take)
+        public IEnumerable<NewsDto> Feed(int skip, int take)
         {
             if (take == 0) take = 10;
 
