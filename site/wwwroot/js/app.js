5a60de9f578bee4a089415e005e60f5eee4d1496
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-27 19:14:17 +0300

    implement back to top button

--- site/wwwroot/js/app.js
+++ site/wwwroot/js/app.js
@@ -123,6 +123,32 @@
 
         if (actualSvc.data.length === 0) actualSvc.read();
     };
+
+    var toTopDirective = function ($window) {
+        var link = function (scope, element, attrs) {
+            var showAfter = parseInt(attrs['showAfter']);
+            if (!showAfter) showAfter = 450;
+
+            element.addClass('hidden');
+
+            angular.element($window).on('scroll', function () {
+                if ($window.document.body.scrollTop > showAfter) {
+                    element.removeClass('hidden');
+                } else {
+                    element.addClass('hidden');
+                }
+            });
+
+            element.on('click', function () {
+                $window.scrollTo(0, 0);
+            });
+        };
+
+        return {
+            restrict: 'E',
+            link: link
+        };
+    };
     
     angular
         .module('timeline', [])
@@ -134,5 +160,7 @@
         .controller('FeedCtrl', ['$scope', 'FeedSvc', 'LoadingSvc', feedCtrl])
 
         .factory('ActualSvc', ['$http', 'LoadingSvc', actualSvc])
-        .controller('ActualCtrl', ['$scope', '$window', 'ActualSvc', 'TypesSvc', actualCtrl]);
+        .controller('ActualCtrl', ['$scope', '$window', 'ActualSvc', 'TypesSvc', actualCtrl])
+
+        .directive('toTop', ['$window', toTopDirective]);
 })();
