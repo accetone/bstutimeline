90fb97ba4496b7d50fd443a4dbc666de526e18ee
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-30 21:44:23 +0300

    refactor angular app: scopes for controllers #4

--- site/wwwroot/js/controller.root.js
+++ site/wwwroot/js/controller.root.js
@@ -2,16 +2,22 @@
 	'use strict';
 
 	var rootCtrl = function ($scope, typesSvc, loadingSvc, newsSvc) {
-	    $scope.types = typesSvc;
-
-	    $scope.showSpinner = function () {
+	    var self = this;
+        
+	    self.showSpinner = function () {
 	        return loadingSvc.is()
                 && ((newsSvc.getData('Feed').length === 0 && typesSvc.isActive('feed')) || (newsSvc.getData('Upcoming').length === 0 && typesSvc.isActive('upcoming')));
 	    };
 
-	    $scope.showSpinnerSmall = function () {
+	    self.showSpinnerSmall = function () {
 	        return loadingSvc.is()
 	            && ((newsSvc.getData('Feed').length > 0 && typesSvc.isActive('feed')) || (newsSvc.getData('Upcoming').length > 0 && typesSvc.isActive('upcoming')));
+	    };
+
+	    $scope.root = {
+	        types: typesSvc,
+	        showSpinner: self.showSpinner,
+            showSpinnerSmall: self.showSpinnerSmall
 	    };
 	};
 
