1bb588984196a73ed60ffec98bfc474f7745acd4
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-30 17:58:25 +0300

    fix spinners

--- site/wwwroot/js/app.js
+++ site/wwwroot/js/app.js
@@ -89,15 +89,15 @@
 	var rootCtrl = function ($scope, typesSvc, loadingSvc, newsSvc) {
 	    $scope.types = typesSvc;
 
-	    /*$scope.showSpinner = function () {
+	    $scope.showSpinner = function () {
 	        return loadingSvc.is()
-                && ((feedSvc.data.length === 0 && typesSvc.isActive('feed')) || (upcomingSvc.data.length === 0 && typesSvc.isActive('upcoming')));
+                && ((newsSvc.getData('Feed').length === 0 && typesSvc.isActive('feed')) || (newsSvc.getData('Upcoming').length === 0 && typesSvc.isActive('upcoming')));
 	    };
 
 	    $scope.showSpinnerSmall = function () {
 	        return loadingSvc.is()
-                && feedSvc.data.length > 0;
-	    };*/
+	            && ((newsSvc.getData('Feed').length > 0 && typesSvc.isActive('feed')) || (newsSvc.getData('Upcoming').length > 0 && typesSvc.isActive('upcoming')));
+	    };
 	};
 
     angular
