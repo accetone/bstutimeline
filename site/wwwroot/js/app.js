478893af3a31459b1553371b729eec0f219e9590
Nikita Tsyhanenko <runninzero@gmail.com>
2016-04-03 17:32:54 +0300

    Google Analytics: check availability of ga #17

--- site/wwwroot/js/app.js
+++ site/wwwroot/js/app.js
@@ -100,8 +100,6 @@
 	    };
 
 	    self.toTopAnalytics = function () {
-	        console.log(3);
-
 	        var category = typesSvc.active[0].toUpperCase() + typesSvc.active.substring(1);
 
 	        analyticsSvc.click(category, 'Back to the top');
@@ -169,7 +167,36 @@
     var analyticsSvc = function ($timeout) {
         var self = this;
 
+        self.trackingCode = 'UA-62450305-3';
         self.counters = {};
+
+        self.queue = [];
+        self.attemp = 0;
+        self.maxAttemps = 15;
+        self.inited = false;
+
+        self.checkAvailability = function() {
+            if (typeof(ga) !== 'function') {
+                self.attemp++;
+
+                if (self.attemp > self.maxAttemps) return;
+
+                $timeout(self.checkAvailability, 1000);
+            } else {
+                ga(self.init);
+            }
+        };
+
+        self.init = function () {
+            ga('create', self.trackingCode, 'auto');
+            ga('send', 'pageview');
+
+            self.inited = true;
+
+            for (var i = 0; i < self.queue; i++) {
+                ga('send', 'event', self.queue[i].category, self.queue[i].type, self.queue[i].label);
+            }
+        };
 
         self.getCounter = function (key) {
             if (self.counters.hasOwnProperty(key)) {
@@ -184,8 +211,20 @@
         self.click = function (category, label) {
             var counter = self.getCounter(category + label);
 
-            ga('send', 'event', category, 'click', label + ' #' + counter);
+            label = label + ' #' + counter;
+
+            if (self.inited) {
+                ga('send', 'event', category, 'click', label);
+            } else {
+                self.queue.push({
+                    type: 'click',
+                    category: category,
+                    label: label
+                });
+            }
         };
+
+        self.init();
 
         return self;
     };
